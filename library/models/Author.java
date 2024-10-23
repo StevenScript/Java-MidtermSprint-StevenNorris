2e3302ac8b1de31272f7a8e8e9340ef555b61fd5
StevenScript <sdn709466@gmail.com>
2024-10-23 11:40:08 -0230

    Improved Author Comments
    Improved commenting on author file to be more descriptive

--- library/models/Author.java
+++ library/models/Author.java
@@ -1,22 +1,41 @@
 package library.models;
 
 /**
- * Author data in our database.
+ * This class represents an author in the library system.
+ * Each author has a name and a date of birth.
  */
 public class Author {
+    // The name of the author
     private String name;
+    
+    // The date of birth of the author, stored as a String
     private String dateOfBirth;
 
+    /**
+     * Constructor to initialize the Author object with a name and date of birth
+     * 
+     * @param name The name of the author.
+     * @param dateOfBirth The date of birth of the author (format "YYYY-MM-DD")
+     */
     public Author(String name, String dateOfBirth) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
     }
 
-    // Getters
+    /**
+     * Returns the name of the author.
+     * 
+     * @return String - author's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the date of birth of the author.
+     * 
+     * @return String - author's date of birth in "YYYY-MM-DD"
+     */
     public String getDateOfBirth() {
         return dateOfBirth;
     }
