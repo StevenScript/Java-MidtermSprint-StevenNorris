503b1fa2e1f6e090e7a34a14ee228c3351a13e54
StevenScript <sdn709466@gmail.com>
2024-10-23 14:27:10 -0230

    Added classes extending Member class
    -created base employee and student files.

---
+++ library/members/employee.java
@@ -0,0 +1,14 @@
+package library.members;
+
+/**
+ * Represents an employee
+ */
+
+public class employee extends member {
+    public employee(String name, String address) {
+        super(name, address);
+    }
+    
+}
+
+ // Additional attributes and methods for Student will be added later
---
+++ library/members/student.java
@@ -0,0 +1,13 @@
+package library.members;
+
+/**
+ * Represents a student patron.
+ */
+public class student extends member {
+
+    public student(String name, String address) {
+        super(name, address);
+    }
+
+    // Additional attributes and methods for Student will be added later
+}
